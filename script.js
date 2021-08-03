39995310923007c2b44ba7e66be47b4a2e229b6f
Binh Truong <truongdacbinh@gmail.com>
2021-08-03 17:02:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const brickInfo = {
 
 function mobileSettings(){
   if (screen.availWidth < 600 ){
-    playGround.width = 375;
+    playGround.width = 340;
     brickInfo.padding = 5;  
     brickInfo.offsetX = 8;
     brickRowCount = 5;
